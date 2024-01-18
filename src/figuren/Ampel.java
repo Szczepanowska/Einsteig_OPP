8d3acff63fb94572c46afea8bbe834cc6da88550
Natalia Szczepanowska <szczepanowska.n@gmail.com>
2024-01-18 12:09:13 +0100

    ampel2

--- src/figuren/Ampel.java
+++ src/figuren/Ampel.java
@@ -12,9 +12,9 @@ public class Ampel {
   public Ampel(int positionX, int positionY, int breite){
     int r = breite/2;
     gehaeuse = new Rechteck(positionX,positionY,breite,3*breite, "schwarz");
-    gruenerKreis = new Kreis(positionX + r, positionY + r, r,"gruen");
+    gruenerKreis = new Kreis(positionX + r, positionY + r+ 2*breite, r,"gruen");
     gelberKreis = new Kreis(positionX + r,positionY + r + breite, r,"gelb");
-    roterKreis = new Kreis(positionX + r,positionY + r + 2*breite, r,"rot");
+    roterKreis = new Kreis(positionX + r,positionY + r, r,"rot");
   }
 
   public Ampel(){
@@ -23,8 +23,13 @@ public class Ampel {
 
   public void zeichne(Leinwand leinwand){
     leinwand.zeichne(gehaeuse);
-    leinwand.zeichne(gruenerKreis);
-    leinwand.zeichne(gelberKreis);
     leinwand.zeichne(roterKreis);
+    leinwand.warte(1000);
+    leinwand.zeichne(gehaeuse);
+    leinwand.zeichne(gelberKreis);
+    leinwand.warte(1000);
+    leinwand.zeichne(gehaeuse);
+    leinwand.zeichne(gruenerKreis);
+    leinwand.warte(1000);
   }
 }
--- src/figuren/Ampelsimulation.java
+++ src/figuren/Ampelsimulation.java
@@ -6,6 +6,8 @@ public class Ampelsimulation {
   public static void main(String[] args) {
     Leinwand leinwand = new Leinwand();
     Ampel ampel = new Ampel();
-    ampel.zeichne(leinwand);
+    while (true) {
+      ampel.zeichne(leinwand);
+    }
   }
 }
